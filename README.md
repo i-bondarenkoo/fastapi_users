d70179b451ad0290ed67e02bcde72ccbd6733b82
Igor Bondarenko <bondarenko200021@mail.ru>
2025-08-28 22:42:37 +0700

    create database config and database helper

---
+++ README.md
@@ -0,0 +1,1 @@
+SQLAlchemy create engine https://docs.sqlalchemy.org/en/20/core/engines.html#sqlalchemy.create_engine
--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 
 
 class RunConfig(BaseModel):
@@ -11,10 +12,19 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 # объект основных настроек
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
